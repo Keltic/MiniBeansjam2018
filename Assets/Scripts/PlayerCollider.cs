6167b586909d6c476ddcf35fb7203461e6ca06d6
tharbas <tharbas@googlemail.com>
2018-05-19 00:21:43 +0200

    Collider, attaching objects to the player on collision

--- Assets/Scripts/PlayerCollider.cs
+++ Assets/Scripts/PlayerCollider.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class PlayerCollider : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject PlayerGameObject;
+
+    const int SpaceTrashLayer = 9;
+    const int AttachToPlayerLayer = 10;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Collider2D thisCollider = this.GetComponent<Collider2D>();
+        if (other.gameObject.layer == SpaceTrashLayer && !thisCollider.usedByEffector)
+        {         
+            other.gameObject.layer = AttachToPlayerLayer;
+            other.gameObject.transform.SetParent(this.gameObject.transform);
+
+            Rigidbody2D body = other.gameObject.GetComponent<Rigidbody2D>();
+            body.velocity = Vector2.zero;
+            body.Sleep();
+            body.WakeUp();
+            Destroy(body);
+            //body.bodyType = RigidbodyType2D.Static;
+            
+            PlayerCollider pc = other.gameObject.AddComponent<PlayerCollider>();
+            pc.PlayerGameObject = PlayerGameObject;
+        }        
+    }
 }
