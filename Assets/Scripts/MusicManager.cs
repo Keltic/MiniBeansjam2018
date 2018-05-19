dbe2714c35167c743ce28bf0a4392bd8b416f17c
tharbas <tharbas@googlemail.com>
2018-05-19 23:27:32 +0200

    MusicManager

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] MusicClips;
+
+    private AudioSource source;
+    int currentTrack;
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        currentTrack = Random.Range(0, MusicClips.Length);
+        source.clip = MusicClips[currentTrack];
+        source.Play();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (source.isPlaying == false)
+        {
+            currentTrack = Random.Range(0, MusicClips.Length);
+            source.clip = MusicClips[currentTrack];
+            source.Play();
+        }
+	}
+}
