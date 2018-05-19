01dffcceaa5e8a7403e192646c23fad6b85ea68c
tharbas <tharbas@googlemail.com>
2018-05-19 16:18:54 +0200

    Adjusting stuff and re-enable angular velocity clamp

--- Assets/Scripts/MagnetComponent.cs
+++ Assets/Scripts/MagnetComponent.cs
@@ -72,6 +72,6 @@ public class MagnetComponent : MonoBehaviour {
         ObjectCollider.isTrigger = IsActive;
 
         //Debug.LogWarning("AV:  " + myBody.velocity);
-        //myBody.angularVelocity = Mathf.Clamp(myBody.angularVelocity, -50f, 50f);
+        myBody.angularVelocity = Mathf.Clamp(myBody.angularVelocity, -50f, 50f);
     }
 }
