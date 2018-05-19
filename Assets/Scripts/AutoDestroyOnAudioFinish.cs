92affd7e2c1eb8bcd81c652c91770002314f9a2f
tharbas <tharbas@googlemail.com>
2018-05-19 22:13:40 +0200

    Explosion SFX for trash -> planet

---
+++ Assets/Scripts/AutoDestroyOnAudioFinish.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroyOnAudioFinish : MonoBehaviour {
+
+    private AudioSource source;
+    private bool hasStarted = false;
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>(); ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted)
+        {
+            if (source.isPlaying)
+            {
+                hasStarted = true;
+            }
+        }
+        else
+        {
+            if (!source.isPlaying)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
--- Assets/Scripts/PlanetSurfaceCollider.cs
+++ Assets/Scripts/PlanetSurfaceCollider.cs
@@ -7,11 +7,19 @@ public class PlanetSurfaceCollider : MonoBehaviour {
     const int SpaceTrashLayer = 9;
     const int AttachToPlayerLayer = 10;
 
+    public GameObject ExplosionSFX;
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
         Collider2D thisCollider = this.GetComponent<Collider2D>();
         if ( (other.gameObject.layer == SpaceTrashLayer && !thisCollider.usedByEffector) || other.gameObject.layer == AttachToPlayerLayer)
         {
+            if (ExplosionSFX != null)
+            {
+                Instantiate(ExplosionSFX);
+            }
+
             Destroy(other.gameObject);
         }
     }
