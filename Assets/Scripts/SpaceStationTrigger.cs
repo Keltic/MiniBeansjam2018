6fb6cf7988865f78cd31ce6c60ef838ad4ffa058
Jan Böhme <jan.boehme@vagabond-software.com>
2018-05-19 18:24:18 +0200

    fix for ontriggerstay

--- Assets/Scripts/SpaceStationTrigger.cs
+++ Assets/Scripts/SpaceStationTrigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpaceStationTrigger : MonoBehaviour
 {
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 10) //10 = AttachedToPlayer
         {
