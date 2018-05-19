86d467f2ec5a26eda5b0ba0bce79b79099ab63fd
tharbas <tharbas@googlemail.com>
2018-05-19 19:36:10 +0200

    Trash will now collide with the planet when attached to the player

--- Assets/Scripts/PlanetSurfaceCollider.cs
+++ Assets/Scripts/PlanetSurfaceCollider.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlanetSurfaceCollider : MonoBehaviour {
 
     const int SpaceTrashLayer = 9;
+    const int AttachToPlayerLayer = 10;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         Collider2D thisCollider = this.GetComponent<Collider2D>();
-        if (other.gameObject.layer == SpaceTrashLayer && !thisCollider.usedByEffector)
+        if ( (other.gameObject.layer == SpaceTrashLayer && !thisCollider.usedByEffector) || other.gameObject.layer == AttachToPlayerLayer)
         {
             Destroy(other.gameObject);
         }
