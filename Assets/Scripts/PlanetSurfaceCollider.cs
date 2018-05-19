87d6b8e349ce97fd8c613c1578f42e6f35781822
tharbas <tharbas@googlemail.com>
2018-05-19 09:05:25 +0200

    Trash is now a prefab. Added trash spawner that spawns around the planet using prefabs

---
+++ Assets/Scripts/PlanetSurfaceCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSurfaceCollider : MonoBehaviour {
+
+    const int SpaceTrashLayer = 9;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Collider2D thisCollider = this.GetComponent<Collider2D>();
+        if (other.gameObject.layer == SpaceTrashLayer && !thisCollider.usedByEffector)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TrashSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashSpawner : MonoBehaviour {
+
+    public GameObject[] TrashSpawnPresets;
+
+    public GameObject CenterPlant;
+
+    public Camera Camera;
+
+    public int InitialSpawnCount = 50;
+
+    public float MinSpawnDistanceFromPlanet = 0.1f;
+    public float MaxSpawnDistanceFromPlanet = 9f;
+
+    private bool init = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (!init)
+        {
+            init = true;
+
+            for (int i = 0; i < InitialSpawnCount; ++i)
+            {
+                Vector2 randomPos = Random.insideUnitCircle;                
+
+                float minSpawn = Mathf.Sqrt(CenterPlant.transform.localScale.x) + MinSpawnDistanceFromPlanet;
+                float maxSpawn = minSpawn + MaxSpawnDistanceFromPlanet;
+                Vector2 spawnPos;
+                spawnPos = GetRandomPointInCircle(Vector2.zero, minSpawn, maxSpawn);
+                //spawnPos.x = minSpawn + (randomPos.x * MaxSpawnDistanceFromPlanet);
+                //spawnPos.y = minSpawn + (randomPos.y * MaxSpawnDistanceFromPlanet);
+
+                int trashIdx = Random.Range(0, TrashSpawnPresets.Length);
+                GameObject trash = Instantiate(TrashSpawnPresets[trashIdx], gameObject.transform);
+                trash.transform.position = new Vector3(spawnPos.x, spawnPos.y, gameObject.transform.position.z);
+            }
+        }
+	}
+
+    Vector2 GetRandomPointInCircle(Vector2 center, float minRadius, float maxRadius)
+    {
+        Vector2 result;
+
+
+        float angle = 2f * Mathf.PI * Random.Range(0, 1f);
+        float radius = Mathf.Sqrt(Random.Range(minRadius, maxRadius));
+
+        result.x = radius * Mathf.Cos(angle) + center.x;
+        result.y = radius * Mathf.Sin(angle) + center.y;
+
+        return result;
+    }
+}
