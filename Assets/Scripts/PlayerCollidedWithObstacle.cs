2315ee64b3c66ca27257b17e3122b157782e5075
tharbas <tharbas@googlemail.com>
2018-05-20 13:47:03 +0200

    Player will now bounce on space station and planet collision

---
+++ Assets/Scripts/PlayerCollidedWithObstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollidedWithObstacle : MonoBehaviour {
+
+    const int PlanetLayer = 13;
+    const int SpaceStationLayer = 14;
+    public float BounceForce = 2000f;
+
+    // Use this for initialization
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.otherCollider.gameObject.layer == PlanetLayer || 
+            collision.otherCollider.gameObject.layer == SpaceStationLayer)
+        {
+            Vector2 myPos = gameObject.transform.position;
+            Vector2 direction = myPos - collision.contacts[0].point;
+
+            Rigidbody2D myBody = collision.otherRigidbody;
+            if (myBody)
+            {
+                myBody.AddForce(direction * BounceForce, ForceMode2D.Impulse);
+            }
+        }
+        
+    }
+}
