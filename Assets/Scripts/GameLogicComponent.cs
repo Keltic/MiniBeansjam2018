add209231c0613276c5921f4ab7a0fb31da0dacf
tharbas <tharbas@googlemail.com>
2018-05-20 16:48:04 +0200

    Tweaking mass display on gameover

--- Assets/Scripts/GameLogicComponent.cs
+++ Assets/Scripts/GameLogicComponent.cs
@@ -26,6 +26,7 @@ public class GameLogicComponent : MonoBehaviour
     private float timer;
     private float points;
     private float overallMass;
+    private float tickedMass;
     private Dictionary<int, int> collectedItems;
     private int crashedRockets;
     private TrashSpawner trashSpawner;
@@ -95,7 +96,14 @@ public class GameLogicComponent : MonoBehaviour
     private void ProcessEndGameTrash(GameObject trash)
     {
         trash.gameObject.layer = 17;
-        trash.GetComponent<Rigidbody2D>().gravityScale = 2.0f;
+        Rigidbody2D body = trash.GetComponent<Rigidbody2D>();
+        body.gravityScale = 2.0f;
+
+        tickedMass += body.mass;
+        string massText = string.Format("{0} tons of trash", tickedMass);
+        this.textMass.text = massText;
+
+
         int childCount = trash.gameObject.transform.childCount;
         for ( int i = 0; i <  childCount; ++i)
         {
@@ -118,7 +126,7 @@ public class GameLogicComponent : MonoBehaviour
             // more == better
             collectedItemsCopy.Add(trash.Key, trash.Value * 5);
         }
-
+        
         foreach (KeyValuePair<int, int> trash in collectedItemsCopy)
         {
             int spawned = 0;
@@ -128,6 +136,8 @@ public class GameLogicComponent : MonoBehaviour
                 int spawnNow = Mathf.Min(left, Random.Range(0, (left /2)+ 1 )) ;
                 this.trashSpawner.SpawnRandomTrashWithMassFilterAtLocation(spawnPos, trash.Key, spawnNow, processAction);
                 spawned += spawnNow;
+                
+                
                 yield return new WaitForSeconds(0.25f);
             }
         }
